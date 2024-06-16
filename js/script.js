e5ec5d1cbc447196172cebf490e1a8832f5d3638
smith109 <devon.smith017@gmail.com>
2024-06-15 20:37:43 -0500

    Add a function to get the computer's choice

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function getRandomNumber() {
   return Math.floor(Math.random() * 3);
 }
+
+function getComputerChoice() {
+  const choices = ['rock', 'paper', 'scissors'];
+  return choices[getRandomNumber()];
+}
