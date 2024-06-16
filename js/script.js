bad7ec7a4dd1347c15d5e20ea38bd37eeaafcde5
smith109 <devon.smith017@gmail.com>
2024-06-15 22:39:51 -0500

    Add a function to get the player's choice

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ function getComputerChoice() {
   const choices = ['rock', 'paper', 'scissors'];
   return choices[getRandomNumber()];
 }
+
+function getPlayerChoice() {
+  let playerChoice = prompt('Please make a selection: rock, paper or scissors');
+
+  if (!playerChoice) {
+    return getPlayerChoice();
+  }
+
+  playerChoice = playerChoice.toLowerCase().trim();
+
+  if (playerChoice === 'rock' ||
+    playerChoice === 'paper' ||
+    playerChoice === 'scissors') {
+    return playerChoice;
+  } else {
+    return getPlayerChoice();
+  }
+}
