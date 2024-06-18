9d81e20828b1010dcb9464dad2744133ec4646e4
smith109 <devon.smith017@gmail.com>
2024-06-17 19:49:49 -0500

    Add function to play a 5 round game

--- js/script.js
+++ js/script.js
@@ -29,21 +29,32 @@ function getPlayerChoice() {
   }
 }
 
-let playerScore = 0;
-let computerScore = 0;
+function playGame() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-function playRound(playerChoice, computerChoice) {
-  if (playerChoice === computerChoice) {
-    console.log(`It's a tie! You both picked ${playerChoice}.`);
+  function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+      console.log(`It's a tie! You both picked ${playerChoice}.`);
+    }
+    else if (playerChoice === 'rock' && computerChoice === 'scissors' ||
+      playerChoice === 'paper' && computerChoice === 'rock' ||
+      playerChoice === 'scissors' && computerChoice === 'paper') {
+      playerScore++;
+      console.log(`You win! ${capitalize(playerChoice)} beats ${computerChoice}.`);
+    }
+    else {
+      computerScore++;
+      console.log(`You lose! ${capitalize(computerChoice)} beats ${playerChoice}.`);
+    }
   }
-  else if (playerChoice === 'rock' && computerChoice === 'scissors' ||
-    playerChoice === 'paper' && computerChoice === 'rock' ||
-    playerChoice === 'scissors' && computerChoice === 'paper') {
-    playerScore++
-    console.log(`You win! ${capitalize(playerChoice)} beats ${computerChoice}.`);
-  }
-  else {
-    computerScore++
-    console.log(`You lose! ${capitalize(computerChoice)} beats ${playerChoice}.`);
+
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    console.log(`Player: ${playerScore} || Computer ${computerScore}`);
   }
 }
+
+playGame();
